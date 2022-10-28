7741d2ce23c460d09ff2fa63966aac9187d7b450
Bjørn-Andre Skaar <31540110+bjornandre@users.noreply.github.com>
2022-10-28 14:16:51 +0200

    Update culling configuration for notebooks (#28)
    * Update culling configuration for notebooks
    
    Internal ticket ID: STAT-152

--- docker/jupyter_notebook_extra_config.py
+++ docker/jupyter_notebook_extra_config.py
@@ -4,6 +4,13 @@ c.NotebookApp.shutdown_no_activity_timeout = 60 * 60
 c.MappingKernelManager.cull_idle_timeout = 30 * 60
 # check for idle kernels every two minutes
 c.MappingKernelManager.cull_interval = 2 * 60
+# Whether to consider culling kernels which have one or more connections.
+c.MappingKernelManager.cull_connected = True
+# Whether to consider culling kernels which are busy.
+c.MappingKernelManager.cull_busy = False
+# Timeout (in seconds) in which a terminal has been inactive and ready to be culled.
+c.TerminalManager.cull_inactive_timeout = 30 * 60
+c.TerminalManager.cull_interval = 2 * 60
 
 c.Application.log_level = 'WARN'
 
