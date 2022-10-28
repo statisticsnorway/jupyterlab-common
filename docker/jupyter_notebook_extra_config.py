493aa69135fdfd98604826b9616233be76e01eb2
Bjørn-Andre Skaar <31540110+bjornandre@users.noreply.github.com>
2022-10-28 10:43:35 +0200

    Update jupyter_notebook_extra_config.py

--- docker/jupyter_notebook_extra_config.py
+++ docker/jupyter_notebook_extra_config.py
@@ -6,6 +6,8 @@ c.MappingKernelManager.cull_idle_timeout = 30 * 60
 c.MappingKernelManager.cull_interval = 2 * 60
 # Whether to consider culling kernels which have one or more connections.
 c.MappingKernelManager.cull_connected = True
+# Whether to consider culling kernels which are busy.
+c.MappingKernelManager.cull_busy = False
 # Timeout (in seconds) in which a terminal has been inactive and ready to be culled.
 c.TerminalManager.cull_inactive_timeout = 30 * 60
 c.TerminalManager.cull_interval = 2 * 60
