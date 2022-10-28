7f6eb7bb7e2c0a1c1da35904196a48dc1fc489e7
Bjørn-Andre Skaar <31540110+bjornandre@users.noreply.github.com>
2022-10-28 18:07:23 +0200

    'shutdown_no_activity_timeout' has moved from NotebookApp to ServerApp

--- docker/jupyter_notebook_extra_config.py
+++ docker/jupyter_notebook_extra_config.py
@@ -1,5 +1,5 @@
 # shutdown the server after no activity for an hour
-c.NotebookApp.shutdown_no_activity_timeout = 60 * 60
+c.ServerApp.shutdown_no_activity_timeout = 60 * 60
 # shutdown kernels after no activity for 30 minutes
 c.MappingKernelManager.cull_idle_timeout = 30 * 60
 # check for idle kernels every two minutes
