ecf1dd9b70165831ced5912dc668e5abd2fdf609
Bjørn-Andre Skaar <31540110+bjornandre@users.noreply.github.com>
2022-11-16 18:01:52 +0100

    Update cull timeouts (#37)

--- docker/jupyter_notebook_extra_config.py
+++ docker/jupyter_notebook_extra_config.py
@@ -1,7 +1,7 @@
-# shutdown the server after no activity for an hour
-c.ServerApp.shutdown_no_activity_timeout = 60 * 60
-# shutdown kernels after no activity for 30 minutes
-c.MappingKernelManager.cull_idle_timeout = 30 * 60
+# shutdown the server after no activity for 8 hours
+c.ServerApp.shutdown_no_activity_timeout = 8 * 60 * 60
+# shutdown kernels after no activity for 1 hour
+c.MappingKernelManager.cull_idle_timeout = 60 * 60
 # check for idle kernels every two minutes
 c.MappingKernelManager.cull_interval = 2 * 60
 # Whether to consider culling kernels which have one or more connections.
@@ -9,7 +9,7 @@ c.MappingKernelManager.cull_connected = True
 # Whether to consider culling kernels which are busy.
 c.MappingKernelManager.cull_busy = False
 # Timeout (in seconds) in which a terminal has been inactive and ready to be culled.
-c.TerminalManager.cull_inactive_timeout = 30 * 60
+c.TerminalManager.cull_inactive_timeout = 60 * 60
 c.TerminalManager.cull_interval = 2 * 60
 
 c.Application.log_level = 'WARN'
